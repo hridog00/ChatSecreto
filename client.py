7336a4845d26c0a470baaf18fbd6036c4bbaabcc
hridog00 <hridog00@estudiantes.unileon.es>
2018-05-15 20:52:47 +0200

    fbs

---
+++ client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = b"Hello, World!"
+M= []
+M.append(0)
+M.append(1)
+bytes(M)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+print (MESSAGE)
+s.send(bytes(M))
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print ("received data:", data)
