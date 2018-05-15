4cbb3b2009495baeecaab8b586b03687daf1d1a1
hridog00 <hridog00@estudiantes.unileon.es>
2018-05-15 21:04:19 +0200

    fbs

--- server.py
+++ server.py
@@ -16,8 +16,7 @@ print ('Connection address:', addr)
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print("Hola soy JUlio y me dejas hacer cambios")
-    print ("received data:", list(data))
+    print ("received data:",data)
     conn.send(data)  # echo
     
 conn.close()
