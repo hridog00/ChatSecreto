a4bad557ef566af1715da4db940dfd45a6ad673e
hridog00 <hridog00@estudiantes.unileon.es>
2018-06-08 21:11:52 +0200

    fbs

--- chat/proyecto/views.py
+++ chat/proyecto/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'proyecto/profile.html')
